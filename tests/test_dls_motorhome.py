05d7dcc63c99ccbe8e2242267bb0c9508540f496
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2020-06-13 23:03:19 +0100

    removed cli.py, and its templated unittest

--- tests/test_dls_motorhome.py
+++ tests/test_dls_motorhome.py
@@ -1,4 +1,4 @@
-from dls_motorhome import HelloClass, cli, say_hello_lots
+from dls_motorhome import HelloClass, say_hello_lots
 
 
 def test_hello_class_formats_greeting() -> None:
@@ -10,11 +10,4 @@ def test_hello_lots_defaults(capsys) -> None:
     say_hello_lots()
     captured = capsys.readouterr()
     assert captured.out == "Hello me\n" * 5
-    assert captured.err == ""
-
-
-def test_cli(capsys) -> None:
-    cli.main(["person", "--times=2"])
-    captured = capsys.readouterr()
-    assert captured.out == "Hello person\n" * 2
     assert captured.err == ""
