a3cadab521f149935e44fa02822940a9035f106b
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2020-06-14 22:27:32 +0100

    added comment __init__.py explaining what __all_ does

--- dls_motorhome/__init__.py
+++ dls_motorhome/__init__.py
@@ -2,4 +2,5 @@ from dls_motorhome._types import HelloClass
 from dls_motorhome._util import say_hello_lots
 from dls_motorhome._version_git import __version__
 
+# __all__ is a list of strings defining what symbols in a module will be exported when from <module> import * is used on the module.
 __all__ = ["__version__", "HelloClass", "say_hello_lots"]
