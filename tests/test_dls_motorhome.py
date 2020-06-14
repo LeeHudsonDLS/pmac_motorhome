a1b24eb685195e04259bbbdb2290553a35c3792c
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2020-06-14 23:23:55 +0100

    removed the tests for the hello world examples

--- tests/test_dls_motorhome.py
+++ tests/test_dls_motorhome.py
@@ -1,13 +1,1 @@
-from dls_motorhome import HelloClass, say_hello_lots
-
-
-def test_hello_class_formats_greeting() -> None:
-    inst = HelloClass("person")
-    assert inst.format_greeting() == "Hello person"
-
-
-def test_hello_lots_defaults(capsys) -> None:
-    say_hello_lots()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello me\n" * 5
-    assert captured.err == ""
+from dls_motorhome import Group
