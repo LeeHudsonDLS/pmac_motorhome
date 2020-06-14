fe8d168bf9543012bb5cf8330b1336f3c206aa48
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2020-06-14 23:23:20 +0100

    removed the hello world examples, and started a new class called Group

--- dls_motorhome/__init__.py
+++ dls_motorhome/__init__.py
@@ -1,7 +1,6 @@
-from dls_motorhome._types import HelloClass
-from dls_motorhome._util import say_hello_lots
+from dls_motorhome._types import Group
 from dls_motorhome._version_git import __version__
 
 # __all__ is a list of strings defining what symbols in a module will be e
 # xported when from <module> import * is used on the module.
-__all__ = ["__version__", "HelloClass", "say_hello_lots"]
+__all__ = ["__version__", "Group"]
