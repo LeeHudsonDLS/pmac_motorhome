385bf5cea0314774c74ed932bc1ce84280292816
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2020-06-15 01:01:50 +0100

    setting plc_number of Group class add axes attribute to Group class

--- dls_motorhome/_types.py
+++ dls_motorhome/_types.py
@@ -1,4 +1,11 @@
 class Group:
+    def __init__(self, *axes, plc_number=9):
+        self.axes = axes
+        self.plc_number = int(plc_number)
+
+        if self.plc_number < 8 or self.plc_number > 32:
+            raise ValueError("plc_number should be integer between 9 and 32")
+
     def __enter__(self):
         pass
 
--- tests/test_dls_motorhome.py
+++ tests/test_dls_motorhome.py
@@ -1,6 +1,29 @@
+import pytest
+
 from dls_motorhome import Group
 
 
 def test_Group_class_is_context_manager():
     assert hasattr(Group(), "__enter__")
     assert hasattr(Group(), "__exit__")
+
+
+def test_Group_class_has_axes_attr():
+    assert hasattr(Group(), "axes")
+
+
+def test_plc_number_set_to_default_if_not_specified():
+    assert Group().plc_number == 9
+
+
+def test_plc_number_set_to_argument_parameter():
+    assert Group(plc_number=10).plc_number == 10
+
+
+def test_plc_number_must_be_within_range():
+    plc_number_min = 8
+    plc_number_max = 32
+    with pytest.raises(ValueError):
+        Group(plc_number=plc_number_max + 1)
+    with pytest.raises(ValueError):
+        Group(plc_number=plc_number_min - 1)
