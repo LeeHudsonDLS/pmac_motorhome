78c3196086ab38907be1cd85a0f078c9a784445d
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2020-06-21 22:22:05 +0100

    corrected typo in comment

--- dls_motorhome/__init__.py
+++ dls_motorhome/__init__.py
@@ -1,6 +1,6 @@
 from dls_motorhome._types import Group
 from dls_motorhome._version_git import __version__
 
-# __all__ is a list of strings defining what symbols in a module will be e
-# xported when from <module> import * is used on the module.
+# __all__ is a list of strings defining what symbols in a module will be
+# exported when from <module> import * is used on the module.
 __all__ = ["__version__", "Group"]
