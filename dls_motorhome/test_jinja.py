f2b3a79b77e4b3783d015b6ae045794f89b7c703
giles <gilesknap@gmail.com>
2020-06-23 12:31:40 +0100

    add motor abstraction and store diff snippet

--- dls_motorhome/test_jinja.py
+++ dls_motorhome/test_jinja.py
@@ -11,14 +11,23 @@ templateLoader = FileSystemLoader(searchpath=jinja_path)
 environment = Environment(loader=templateLoader)
 
 
-# TODO IMPORTANT - this is used in finding the Home capture flags etc. and is
-# specific to Geobrick - you will want to use a Motor abstraction instead
-# of just a list of motor numbers. For inspiration see Motor class in
-#  ... pmacutil/pmacUtilApp/src/motorhome.py
-# HINT: watch out for python 2 vs python 3 handling of integer arithmetic
-def axis_nx(ax: int) -> str:
-    nx = int(int((ax - 1) / 4) * 10 + int((ax - 1) % 4 + 1))
-    return "{:02}".format(nx)
+class Motor:
+    instances: List["Motor"] = []
+
+    def __init__(self, axis: int, jdist: int) -> None:
+        self.axis = axis
+        self.jdist = jdist
+        self.index = len(self.instances)
+        self.instances.append(self)
+
+    # TODO IMPORTANT - this is used in finding the Home capture flags etc. and is
+    # specific to Geobrick - For full implementation see Motor class in
+    #  ... pmacutil/pmacUtilApp/src/motorhome.py
+    # HINT: watch out for python 2 vs python 3 handling of integer arithmetic
+    @property
+    def nx(self) -> str:
+        nx = int(int((self.axis - 1) / 4) * 10 + int((self.axis - 1) % 4 + 1))
+        return "{:02}".format(nx)
 
 
 class SnippetGenerator:
@@ -48,12 +57,13 @@ class Snippet(Enum):
     pre_home = "pre_home_move"
     fast_search = "fast_search"
     debug = "debug_pause"
+    store_pos = "store_position_diff"
 
 
 class Group:
     # TODO this probably needs to be merged into your Group class that implements
     # the python contexts
-    def __init__(self, axes: List[int], group_num: int = 9) -> None:
+    def __init__(self, axes: List[Motor], group_num: int = 9) -> None:
         self.axes = axes
         self.comments: str = f"; TODO commments placeholder for group {group_num}"
         self.group_num = group_num
@@ -64,7 +74,7 @@ class Group:
         return len(self.snippets)
 
     def _all_axes(self, template: str, separator: str) -> str:
-        all = [template.format(axis) for axis in self.axes]
+        all = [template.format(ax.axis) for ax in self.axes]
         return separator.join(all)
 
     def jog_axes(self) -> str:
@@ -85,12 +95,21 @@ class Group:
     def clear_home(self) -> str:
         return self._all_axes("m{0}45=0", " ")
 
+    def store_position_diff(self):
+        return "\n        ".join(
+            [
+                f"P{ax.index+1184}=(P{ax.index+1184}-M{ax.axis}62)"
+                f"/(I{ax.axis}08*32)+{ax.jdist}-(i{ax.axis}26/16)"
+                for ax in self.axes
+            ]
+        )
+
 
 class Plc:
     def __init__(self, plc_num: int) -> None:
         self.plc_num = plc_num
         self.groups: List[Group] = []
-        self.axes: List[int] = []
+        self.axes: List[Motor] = []
 
     def add_group(self, group: Group):
         self.groups.append(group)
@@ -101,58 +120,58 @@ class Plc:
         return len(self.groups)
 
     def save_hi_limits(self):
-        return " ".join([f"P{n+1104}=i{ax}13" for n, ax in enumerate(self.axes)])
+        return " ".join([f"P{ax.index+1104}=i{ax.axis}13" for ax in self.axes])
 
     def restore_hi_limits(self):
-        return " ".join([f"i{ax}13=P{n+1104}" for n, ax in enumerate(self.axes)])
+        return " ".join([f"i{ax.axis}13=P{ax.index+1104}" for ax in self.axes])
 
     def save_lo_limits(self):
-        return " ".join([f"P{n+1120}=i{ax}14" for n, ax in enumerate(self.axes)])
+        return " ".join([f"P{ax.index+1120}=i{ax.axis}14" for ax in self.axes])
 
     def restore_lo_limits(self):
-        return " ".join([f"i{ax}14=P{n+1120}" for n, ax in enumerate(self.axes)])
+        return " ".join([f"i{ax.axis}14=P{ax.index+1120}" for ax in self.axes])
 
     def save_homed(self):
-        return " ".join(
-            [f"P{n+1136}=i7{axis_nx(ax)}2" for n, ax in enumerate(self.axes)]
-        )
+        return " ".join([f"P{ax.index+1136}=i7{ax.nx}2" for ax in self.axes])
 
     def restore_homed(self):
-        return " ".join(
-            [f"i7{axis_nx(ax)}2=P{n+1136}" for n, ax in enumerate(self.axes)]
-        )
+        return " ".join([f"i7{ax.nx}2=P{ax.index+1136}" for ax in self.axes])
 
     def save_not_homed(self):
-        return " ".join([f"P{n+1152}=P{n+1136}^$C" for n, ax in enumerate(self.axes)])
+        return " ".join([f"P{ax.index+1152}=P{ax.index+1136}^$C" for ax in self.axes])
 
     def save_limit_flags(self):
-        return " ".join([f"P{n+1168}=i{ax}24" for n, ax in enumerate(self.axes)])
+        return " ".join([f"P{ax.index+1168}=i{ax.axis}24" for ax in self.axes])
 
     def restore_limit_flags(self):
-        return " ".join([f"i{ax}24=P{n+1168}" for n, ax in enumerate(self.axes)])
+        return " ".join([f"i{ax.axis}24=P{ax.index+1168}" for ax in self.axes])
 
     def save_position(self):
-        return " ".join([f"P{n+1184}=M{ax}62" for n, ax in enumerate(self.axes)])
+        return " ".join([f"P{ax.index+1184}=M{ax.axis}62" for ax in self.axes])
 
     def clear_limits(self):
-        r = " ".join([f"i{ax}13=0" for ax in self.axes])
+        r = " ".join([f"i{ax.axis}13=0" for ax in self.axes])
         r += "\n"
-        r += " ".join([f"i{ax}14=0" for ax in self.axes])
+        r += " ".join([f"i{ax.axis}14=0" for ax in self.axes])
         return r
 
     def stop_motors(self):
         return "\n".join(
-            [f'if (m{ax}42=0)\n    cmd "#{ax}J/"\nendif' for ax in self.axes]
+            [f'if (m{ax.axis}42=0)\n    cmd "#{ax.axis}J/"\nendif' for ax in self.axes]
         )
 
 
-# TODO all this will magically happen in the 'with Group' statements
+# TODO all this below will magically happen in the 'with Group' statements
 s = SnippetGenerator()
 p = Plc(11)
 
-g1 = Group([1, 2], 2)
-g1.snippets = [Snippet.pre_home, Snippet.debug]
-g2 = Group([4, 5], 3)
+m1 = Motor(axis=1, jdist=0)
+m2 = Motor(axis=2, jdist=0)
+m4 = Motor(axis=4, jdist=0)
+m5 = Motor(axis=5, jdist=0)
+g1 = Group([m1, m2], 2)
+g1.snippets = [Snippet.pre_home, Snippet.debug, Snippet.store_pos]
+g2 = Group([m4, m5], 3)
 g2.snippets = [Snippet.pre_home, Snippet.debug]
 
 p.add_group(g1)
