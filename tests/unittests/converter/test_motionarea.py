b2cdf56aa4877920b635df34314e802e69228623
UrszulaNeuman <urszula.neuman@gmail.com>
2022-04-08 13:57:54 +0100

    fix test layout

--- tests/unittests/converter/test_motionarea.py
+++ tests/unittests/converter/test_motionarea.py
@@ -7,15 +7,14 @@ from converter.motionarea import MotionArea
 
 
 class TestMotionArea(unittest.TestCase):
-
-    @patch('subprocess.check_output')
+    @patch("subprocess.check_output")
     def test_shebang_looks_as_expected(self, mock_subprocess):
         # Arrange
-        python_path = '/bin/python'
-        expected_shebang = '#!/bin/env /bin/python \n'
+        python_path = "/bin/python"
+        expected_shebang = "#!/bin/env /bin/python \n"
         mock_subprocess.return_value = python_path.encode("UTF-8")
         stream = StringIO()
-        motionarea = MotionArea(Path('/tmp'))
+        motionarea = MotionArea(Path("/tmp"))
         # Act
         motionarea.write_shebang(stream)
         stream.seek(0)  # Change the stream position to start of the stream
